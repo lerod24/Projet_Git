c84a843ef5e143ad999a7eb6acac15f5c56369c3
rodrigue <kocherodriguez@yahoo.fr>
2024-06-05 01:36:12 +0200

    fichiers a jour

--- fonctions.py
+++ fonctions.py
@@ -2,15 +2,15 @@
 
 
 def facto(n):
-    if n < 0:
-        print ("Le factoriel n'est défini que pour les entiers naturels.")
-    elif n == 0:
-        print("Par convention factoriel de '0' égal à '1' ")
+    #if n < 0:
+        #print ("Le factoriel n'est défini que pour les entiers naturels.")
+    if n == 0:
+        #print("Par convention factoriel de '0' égal à '1' ")
         return 1
     elif n !=0:
         resultat = 1
         for i in range(1, n + 1):
-            resultat = resultat*i
+            resultat *=i
             
         return resultat
 
